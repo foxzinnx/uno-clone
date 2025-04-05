eafdc9ba44762b9a0cb1a80039652ffd4a8ada29
foxz1n <bryangomes16624@gmail.com>
2025-04-05 19:33:47 -0300

    Update: Header & Products section

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -14,10 +14,10 @@ export const Header = () => {
                 />
                 <div className="hidden lg:flex items-center gap-9">
                     <ul className="flex gap-7 text-white text-md">
-                        <li>Inicio</li>
-                        <li>Onde Encontrar</li>
-                        <li>Novos Produtos</li>
-                        <li>Créditos</li>
+                        <li className="cursor-pointer hover:text-red-500 transition-all duration-300">Inicio</li>
+                        <li className="cursor-pointer hover:text-red-500 transition-all duration-300">Onde Encontrar</li>
+                        <li className="cursor-pointer hover:text-red-500 transition-all duration-300">Novos Produtos</li>
+                        <li className="cursor-pointer hover:text-red-500 transition-all duration-300">Créditos</li>
                     </ul>
                     <button className="w-30 h-11 border-2 border-white text-white rounded-xl cursor-pointer hover:bg-red-500 hover:text-white transition-all duration-300 hover:border-red-500 hover:shadow-[0_0_15px_5px_rgba(239,68,68,0.7)]">Entrar</button>
                     <p className="text-white hover:text-red-500 cursor-pointer transition-all duration-300">Cadastre-se</p>
--- src/components/ui/products.tsx
+++ src/components/ui/products.tsx
@@ -8,9 +8,9 @@ export const Products = () => {
             <div className="max-w-[1300px] w-full px-4 flex flex-col justify-center items-center">
                 <div className="flex justify-between items-center w-full">
                     <h1 className="text-white font-medium border-b-2 border-b-[#666666] pb-4 text-3xl">Escolha o seu modelo</h1>
-                    <div className="bg-[#ed1c24] flex justify-center items-center rounded-xl w-[8rem] gap-2 h-12">
-                        <FontAwesomeIcon icon={faFilter} className="text-white size-5" />
-                       <button className="text-white font-semibold">Filtrar</button> 
+                    <div className="bg-[#ed1c24] flex justify-center items-center rounded-xl cursor-pointer hover:bg-white transition-all duration-300 group w-[8rem] gap-2 h-12">
+                        <FontAwesomeIcon icon={faFilter} className="text-white size-5 group-hover:text-red-500" />
+                       <button className="text-white font-semibold group-hover:text-red-500 cursor-pointer">Filtrar</button> 
                     </div>
                 </div>
 
@@ -23,15 +23,20 @@ export const Products = () => {
                                 alt=""
                                 width={270}
                                 height={270}
+                                className="hover:scale-105 transition-all duration-500 cursor-pointer"
                             />
                         </div>
                         <div className="w-full border-b border-b-white pb-3 flex px-3 justify-between pt-5 items-center">
                             <h1 className="text-white font-medium text-xl ">UNO® Flip SIOC</h1>
                             <p className="text-white text-lg">R$ 104,99</p>
                         </div>
-                        <div className="bg-[#ed1c24] flex justify-center gap-2 items-center mt-5 rounded-xl w-full h-13">
-                            <FontAwesomeIcon icon={faBagShopping} className="text-white size-5" />
-                            <button className="font-semibold text-lg text-white">Comprar</button>
+                        <div className="bg-[#ed1c24] relative flex cursor-pointer justify-center gap-2 items-center mt-5 rounded-xl w-full h-13 overflow-hidden group">
+                            <div className="absolute inset-0 bg-red-800 transform translate-x-[-100%] group-hover:translate-x-0 transition-transform duration-1200 ease-in-out"></div>
+                            
+                            <div className="relative z-10 flex justify-center items-center gap-2 w-full">
+                                <FontAwesomeIcon icon={faBagShopping} className="text-white size-5" />
+                                <button className="font-semibold text-lg cursor-pointer text-white">Comprar</button>
+                            </div>
                         </div>
                     </div>
 
@@ -42,15 +47,20 @@ export const Products = () => {
                                 alt=""
                                 width={270}
                                 height={270}
+                                className="hover:scale-105 transition-all duration-500 cursor-pointer"
                             />
                         </div>
                         <div className="w-full border-b border-b-white pb-3 flex px-3 justify-between pt-5 items-center">
                             <h1 className="text-white font-medium text-xl ">UNO® Dos</h1>
                             <p className="text-white text-lg">R$ 50,00</p>
                         </div>
-                        <div className="bg-[#ed1c24] flex justify-center gap-2 items-center mt-5 rounded-xl w-full h-13">
-                            <FontAwesomeIcon icon={faBagShopping} className="text-white size-5" />
-                            <button className="font-semibold text-lg text-white">Comprar</button>
+                        <div className="bg-[#ed1c24] relative flex cursor-pointer justify-center gap-2 items-center mt-5 rounded-xl w-full h-13 overflow-hidden group">
+                            <div className="absolute inset-0 bg-red-800 transform translate-x-[-100%] group-hover:translate-x-0 transition-transform duration-1200 ease-in-out"></div>
+                            
+                            <div className="relative z-10 flex justify-center items-center gap-2 w-full">
+                                <FontAwesomeIcon icon={faBagShopping} className="text-white size-5" />
+                                <button className="font-semibold text-lg cursor-pointer text-white">Comprar</button>
+                            </div>
                         </div>
                     </div>
 
@@ -61,15 +71,20 @@ export const Products = () => {
                                 alt=""
                                 width={270}
                                 height={270}
+                                className="hover:scale-105 transition-all duration-500 cursor-pointer"
                             />
                         </div>
                         <div className="w-full border-b border-b-white pb-3 flex px-3 justify-between pt-5 items-center">
                             <h1 className="text-white font-medium text-xl ">UNO® Original</h1>
                             <p className="text-white text-lg">R$ 29,99</p>
                         </div>
-                        <div className="bg-[#ed1c24] flex justify-center gap-2 items-center mt-5 rounded-xl w-full h-13">
-                            <FontAwesomeIcon icon={faBagShopping} className="text-white size-5" />
-                            <button className="font-semibold text-lg text-white">Comprar</button>
+                        <div className="bg-[#ed1c24] relative flex cursor-pointer justify-center gap-2 items-center mt-5 rounded-xl w-full h-13 overflow-hidden group">
+                            <div className="absolute inset-0 bg-red-800 transform translate-x-[-100%] group-hover:translate-x-0 transition-transform duration-1200 ease-in-out"></div>
+                            
+                            <div className="relative z-10 flex justify-center items-center gap-2 w-full">
+                                <FontAwesomeIcon icon={faBagShopping} className="text-white size-5" />
+                                <button className="font-semibold text-lg cursor-pointer text-white">Comprar</button>
+                            </div>
                         </div>
                     </div>
 
@@ -80,15 +95,20 @@ export const Products = () => {
                                 alt=""
                                 width={270}
                                 height={270}
+                                className="hover:scale-105 transition-all duration-500 cursor-pointer"
                             />
                         </div>
                         <div className="w-full border-b border-b-white pb-3 flex px-3 justify-between pt-5 items-center">
                             <h1 className="text-white font-medium text-xl ">UNO® Masters</h1>
                             <p className="text-white text-lg">R$ 34,99</p>
                         </div>
-                        <div className="bg-[#ed1c24] flex justify-center gap-2 items-center mt-5 rounded-xl w-full h-13">
-                            <FontAwesomeIcon icon={faBagShopping} className="text-white size-5" />
-                            <button className="font-semibold text-lg text-white">Comprar</button>
+                        <div className="bg-[#ed1c24] relative flex cursor-pointer justify-center gap-2 items-center mt-5 rounded-xl w-full h-13 overflow-hidden group">
+                            <div className="absolute inset-0 bg-red-800 transform translate-x-[-100%] group-hover:translate-x-0 transition-transform duration-1200 ease-in-out"></div>
+                            
+                            <div className="relative z-10 flex justify-center items-center gap-2 w-full">
+                                <FontAwesomeIcon icon={faBagShopping} className="text-white size-5" />
+                                <button className="font-semibold text-lg cursor-pointer text-white">Comprar</button>
+                            </div>
                         </div>
                     </div>
 
@@ -99,15 +119,20 @@ export const Products = () => {
                                 alt=""
                                 width={270}
                                 height={270}
+                                className="hover:scale-105 transition-all duration-500 cursor-pointer"
                             />
                         </div>
                         <div className="w-full border-b border-b-white pb-3 flex px-3 justify-between pt-5 items-center">
                             <h1 className="text-white font-medium text-xl ">UNO® Barbie</h1>
                             <p className="text-white text-lg">R$ 23,99</p>
                         </div>
-                        <div className="bg-[#ed1c24] flex justify-center gap-2 items-center mt-5 rounded-xl w-full h-13">
-                            <FontAwesomeIcon icon={faBagShopping} className="text-white size-5" />
-                            <button className="font-semibold text-lg text-white">Comprar</button>
+                        <div className="bg-[#ed1c24] relative flex cursor-pointer justify-center gap-2 items-center mt-5 rounded-xl w-full h-13 overflow-hidden group">
+                            <div className="absolute inset-0 bg-red-800 transform translate-x-[-100%] group-hover:translate-x-0 transition-transform duration-1200 ease-in-out"></div>
+                            
+                            <div className="relative z-10 flex justify-center items-center gap-2 w-full">
+                                <FontAwesomeIcon icon={faBagShopping} className="text-white size-5" />
+                                <button className="font-semibold text-lg cursor-pointer text-white">Comprar</button>
+                            </div>
                         </div>
                     </div>
 
@@ -118,15 +143,20 @@ export const Products = () => {
                                 alt=""
                                 width={270}
                                 height={270}
+                                className="hover:scale-105 transition-all duration-500 cursor-pointer"
                             />
                         </div>
                         <div className="w-full border-b border-b-white pb-3 flex px-3 justify-between pt-5 items-center">
                             <h1 className="text-white font-medium text-xl ">UNO® Masters</h1>
                             <p className="text-white text-lg">R$ 34,99</p>
                         </div>
-                        <div className="bg-[#ed1c24] flex justify-center gap-2 items-center mt-5 rounded-xl w-full h-13">
-                            <FontAwesomeIcon icon={faBagShopping} className="text-white size-5" />
-                            <button className="font-semibold text-lg text-white">Comprar</button>
+                        <div className="bg-[#ed1c24] relative flex cursor-pointer justify-center gap-2 items-center mt-5 rounded-xl w-full h-13 overflow-hidden group">
+                            <div className="absolute inset-0 bg-red-800 transform translate-x-[-100%] group-hover:translate-x-0 transition-transform duration-1200 ease-in-out"></div>
+                            
+                            <div className="relative z-10 flex justify-center items-center gap-2 w-full">
+                                <FontAwesomeIcon icon={faBagShopping} className="text-white size-5" />
+                                <button className="font-semibold text-lg cursor-pointer text-white">Comprar</button>
+                            </div>
                         </div>
                     </div>
 
@@ -137,15 +167,20 @@ export const Products = () => {
                                 alt=""
                                 width={270}
                                 height={270}
+                                className="hover:scale-105 transition-all duration-500 cursor-pointer"
                             />
                         </div>
                         <div className="w-full border-b border-b-white pb-3 flex justify-between pt-5 items-center">
                             <h1 className="text-white font-medium text-xl ">UNO® Minimalista</h1>
                             <p className="text-white text-lg">R$ 54,99</p>
                         </div>
-                        <div className="bg-[#ed1c24] flex justify-center gap-2 items-center mt-5 rounded-xl w-full h-13">
-                            <FontAwesomeIcon icon={faBagShopping} className="text-white size-5" />
-                            <button className="font-semibold text-lg text-white">Comprar</button>
+                        <div className="bg-[#ed1c24] relative flex cursor-pointer justify-center gap-2 items-center mt-5 rounded-xl w-full h-13 overflow-hidden group">
+                            <div className="absolute inset-0 bg-red-800 transform translate-x-[-100%] group-hover:translate-x-0 transition-transform duration-1200 ease-in-out"></div>
+                            
+                            <div className="relative z-10 flex justify-center items-center gap-2 w-full">
+                                <FontAwesomeIcon icon={faBagShopping} className="text-white size-5" />
+                                <button className="font-semibold text-lg cursor-pointer text-white">Comprar</button>
+                            </div>
                         </div>
                     </div>
 
@@ -156,15 +191,20 @@ export const Products = () => {
                                 alt=""
                                 width={270}
                                 height={270}
+                                className="hover:scale-105 transition-all duration-500 cursor-pointer"
                             />
                         </div>
                         <div className="w-full border-b border-b-white pb-3 flex px-3 justify-between pt-5 items-center">
                             <h1 className="text-white font-medium text-xl ">UNO® Flip</h1>
                             <p className="text-white text-lg">R$ 54,99</p>
                         </div>
-                        <div className="bg-[#ed1c24] flex justify-center gap-2 items-center mt-5 rounded-xl w-full h-13">
-                            <FontAwesomeIcon icon={faBagShopping} className="text-white size-5" />
-                            <button className="font-semibold text-lg text-white">Comprar</button>
+                        <div className="bg-[#ed1c24] relative flex cursor-pointer justify-center gap-2 items-center mt-5 rounded-xl w-full h-13 overflow-hidden group">
+                            <div className="absolute inset-0 bg-red-800 transform translate-x-[-100%] group-hover:translate-x-0 transition-transform duration-1200 ease-in-out"></div>
+                            
+                            <div className="relative z-10 flex justify-center items-center gap-2 w-full">
+                                <FontAwesomeIcon icon={faBagShopping} className="text-white size-5" />
+                                <button className="font-semibold text-lg cursor-pointer text-white">Comprar</button>
+                            </div>
                         </div>
                     </div>
                 </div>
